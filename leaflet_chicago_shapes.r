627872c4edc34ff8faab8dc08b618c4e31d9a814
jason-and <andrew.r.jason@gmail.com>
2025-04-23 13:18:35 -0500

    fixed to correct r file

--- leaflet_chicago_shapes.r
+++ leaflet_chicago_shapes.r
@@ -1,14 +1,31 @@
-library(httr)
 library(sf)
 library(leaflet)
 
-url <- parse_url("https://gis.cookcountyil.gov/traditional/rest/services/politicalBoundary/MapServer/9")
+# URL to the GeoJSON file from Cook County Open Data Portal
+url <- "https://hub-cookcountyil.opendata.arcgis.com/datasets/8eb3dca457cb46d7a2a0c96588c4016c_9.geojson"
 
-url$query <- list(outFields = "*",
-                  returnGeometry = "true",
-                  f = "geojson",
-                  where = "1=1")
+# Download the file
+download.file(url, "cook_county_districts.geojson", mode = "wb")
 
-request <- build_url(url)
+# Read the GeoJSON file
+districts <- st_read("cook_county_districts.geojson")
 
-districts <- read_sf(request)
+# Check the output
+print(head(districts))
+
+# Create a map
+map <- leaflet(districts) %>%
+        addProviderTiles(providers$CartoDB.Positron) %>%
+        addPolygons(
+                fillColor = ~ colorNumeric("viridis", DISTRICT_INT)(DISTRICT_INT),
+                weight = 1,
+                opacity = 1,
+                color = "white",
+                dashArray = "3",
+                fillOpacity = 0.4,
+                popup = ~ paste0("<b>District: </b>", DISTRICT_TXT)
+        )
+
+# Save the map to an HTML file
+htmlwidgets::saveWidget(map, "cook_county-districts_map.html")
+
